ff99b16370ad9c437e17229db33b0e4e6a3277ae
andre <andrecabralpires@gmail.com>
2022-11-14 09:50:40 +0000

    added 3 new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb "path", to: "controller#action"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  root "pages#home"
 end
